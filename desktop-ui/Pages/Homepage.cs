9810f6b96119aba4c5546e4281ca0d693cf9dd4c
Josh <hi@oracularhades.com>
2024-05-29 10:19:55 +1200

    Changes to spacing and phrasing

--- desktop-ui/Pages/Homepage.cs
+++ desktop-ui/Pages/Homepage.cs
@@ -6,7 +6,7 @@ namespace rover_desktop_ui
 {
     public partial class HomePage : ContentPage
     {
-        private List<string> _consented = new List<string> { "View applications running on your system", "View installed applications", "View list of system users", "Apply policies to applications", "Scan for confidential documents, but not read your documents", "Control where data can be moved from or into this device (e.g, controlling if a file can be placed on a USB, if it can copy/paste.)" };
+        private List<string> _consented = new List<string> { "View applications running on your system", "View installed applications", "View list of system users", "Apply policies to applications", "Scan for confidential documents, but not read your documents", "Control where data can be moved from or into this device (e.g, controlling if a file can be placed on a USB, if it can copy/paste)" };
         public List<string> Consented
         {
             get => _consented;
